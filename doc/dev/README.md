95387882852821166df6788fa03e32089e17f8de
fast-ionic <fast-ionic@gitub.com>
2019-06-26 16:47:30 +0800

    v0.0.1 初次提交

---
+++ doc/dev/README.md
@@ -0,0 +1,6 @@
+# 二次开发
+
+- 下载’git@github.com:wmzwq/Fast-ionic.git‘
+- 编辑’src‘目录的源码
+- 执行’npm run release‘编译
+- 执行’npm run example‘启动示例
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert('faste-ionic');
+class FastCache{
+    constructor(){
+        this.list={}
+    }
+    set(key,value){
+        this.list[key]=value
+    }
+    get(key){
+        return this.list[key]
+    }
+}
+
+windows.FastCache=FastCache
